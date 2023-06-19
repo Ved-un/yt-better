49121905bc5915b3984d3a50a0fd8ba356a07ac5
CheemsBread <dogecheemsbread@gmail.com>
2023-06-19 14:44:37 -0400

    Base scripts
    Just first upload of the scripts

---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+I made this bash script to fix the few issues I have with yt-dlp. 
+
+Just clone the repo and run "chmod +x install.sh && chmod +x yt-better" to make the scripts runnable and then run "sudo ./install.sh" and it should install and use it like normal. 
---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Define the source and destination paths
+source_file="yt"
+source_file_music="yt-music"
+destination_dir="/usr/bin"
+
+# chmod
+chmod +x $source_file
+chmod +x $source_file_music
+
+## yt
+# Check if the file exists
+if [ -f "$source_file" ]; then
+    # Move the file to the destination directory
+    mv "$source_file" "$destination_dir"
+
+    # Check if the move operation was successful
+    if [ $? -eq 0 ]; then
+        echo "File moved successfully to $destination_dir."
+    else
+        echo "Failed to move the file."
+    fi
+else
+    echo "File '$source_file' not found."
+fi
+
+# yt music
+# Check if the file exists
+if [ -f "$source_file_music" ]; then
+    # Move the file to the destination directory
+    mv "$source_file_music" "$destination_dir"
+
+    # Check if the move operation was successful
+    if [ $? -eq 0 ]; then
+        echo "File moved successfully to $destination_dir."
+    else
+        echo "Failed to move the file."
+    fi
+else
+    echo "File '$source_file_music' not found."
+fi
